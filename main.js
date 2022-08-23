d4932932d954b071c282ce2942c1815e28f2ab57
Joanna Makarewicz <joanna.milun@gmail.com>
2022-08-23 13:06:12 +0200

    Adding pseudoelement 'before'

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger--js');
 hamburger.addEventListener('click', () => {
     const nav = document.querySelector('.navigation--js');
-    nav.classList.toggle('navigation--open');
+    nav.classList.toggle('navigation__open');
 })
