8f3ef3e7a33afad35aa639680092b417da1885b8
Joanna Makarewicz <joanna.milun@gmail.com>
2022-08-25 12:21:02 +0200

    Adding H1

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ hamburger.addEventListener('click', () => {
     const nav = document.querySelector('.navigation--js');
     nav.classList.toggle('navigation__open');
 })
+
+console.log(`hello`);
+
+const adress = (town) => {
+    return `Moje miasto to ${town}`;
+}
+
+console.log(adress(`Gdańsk`));
